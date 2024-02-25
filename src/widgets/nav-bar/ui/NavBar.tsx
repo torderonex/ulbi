ff6bd13e38f913e19e8a847b2d6969fb10ed39cf
torderonex <eligorko.info@yandex.ru>
2024-02-25 20:00:32 +0300

    sidebar navigation

--- src/widgets/nav-bar/ui/NavBar.tsx
+++ src/widgets/nav-bar/ui/NavBar.tsx
@@ -1,21 +1,16 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './navbar.module.scss';
-import  { AppLink, AppLinkTheme } from 'shared/ui/';
-import { useTranslation } from 'react-i18next';
 
 interface NavbarProps{
   className? : string, 
 }
 
 export default function NavBar({className} : NavbarProps) {
-	const {t} = useTranslation();
 	return (
 		<div className={classNames(styles.navbar,{},[className])}>
 			<div className={classNames(styles.links)}>
-				<AppLink to={'/'} className={styles.mainLink}>{t('Main')}</AppLink>
-				<AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t('About')}</AppLink>
+				{'/'}
 			</div>
-      
 		</div>
 	);
 }
--- src/widgets/sidebar/ui/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar/Sidebar.tsx
@@ -6,6 +6,10 @@ import { LangSwitcher } from 'widgets/lang-switcher';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui';
 import { ButtonSize, ButtonTheme } from 'shared/ui/button/Button';
+import {AppLink} from 'shared/ui';
+import AboutSvg from 'shared/assets/icons/about.svg';
+import HomeSvg from 'shared/assets/icons/home.svg';
+import { routePath } from 'shared/config/route-config/routeConfig';
 
 interface SidebarProps{
   className? : string,
@@ -31,6 +35,16 @@ export default function Sidebar({className} : SidebarProps) {
 			>
 				{collapsed ? '>' : '<'}
 			</Button>
+			<div className={styles.items}>
+				<AppLink to={routePath.main} className={styles.item}>
+					<HomeSvg className={styles.ico} width={'30px'} height={'30px'}/>
+					<span className={styles.link}>{t('Main')}</span>
+				</AppLink>
+				<AppLink className={styles.item}  to={routePath.about}>
+					<AboutSvg className={styles.ico} width={'30px'} height={'30px'}/>
+					<span className={styles.link}>{t('About')}</span>
+				</AppLink>
+			</div>
 			<div className={styles.switches}>
 				<ThemeSwitcher/>
 				<LangSwitcher 
